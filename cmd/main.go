a96ed72800bfae8ae48e7326ea90110240c992f4
ramnoah <castillo_noe21@hotmail.com>
2024-10-28 23:00:43 -0600

    added video templates practice

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Name  string
+	Email string
+}
 
 func main() {
-	fmt.Println("Works")
+	r := gin.Default()
+
+	user := &User{
+		Name:  "Noe",
+		Email: "ramnoah@gmail.com",
+	}
+
+	users := []User{
+		{
+			Name:  "Mario",
+			Email: "mar@mail.com",
+		},
+		{
+			Name:  "Fer",
+			Email: "fer@gmail.com",
+		},
+	}
+
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title":      "Main website",
+			"totalUsers": 11,
+			"user":       user,
+			"users":      users,
+		})
+	})
+	r.Run(":8001")
+	fmt.Println("Running")
 }
